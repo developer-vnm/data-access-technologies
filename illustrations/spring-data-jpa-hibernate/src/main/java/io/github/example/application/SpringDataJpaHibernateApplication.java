3c7f097ecf67d061c3f0b941f99ced159d3e9316
Developer <developer.sharpening@gmail.com>
2024-11-19 23:02:25 +0700

    style: optimized import

--- illustrations/spring-data-jpa-hibernate/src/main/java/io/github/example/application/SpringDataJpaHibernateApplication.java
+++ illustrations/spring-data-jpa-hibernate/src/main/java/io/github/example/application/SpringDataJpaHibernateApplication.java
@@ -1,14 +1,10 @@
 package io.github.example.application;
 
 import io.github.example.application.services.ItemService;
-import jakarta.annotation.PostConstruct;
-import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.SessionFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
@@ -16,14 +12,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SpringDataJpaHibernateApplication implements CommandLineRunner {
 
-	private final List<ItemService> itemServices;
+    private final List<ItemService> itemServices;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDataJpaHibernateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaHibernateApplication.class, args);
+    }
 
-	@Override
-	public void run(final String... args) {
-		itemServices.forEach(itemService -> itemService.increaseAmount(1L, 10L));
-	}
+    @Override
+    public void run(final String... args) {
+        itemServices.forEach(itemService -> itemService.increaseAmount(1L, 10L));
+    }
 }
